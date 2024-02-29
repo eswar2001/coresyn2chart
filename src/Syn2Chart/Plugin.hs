a99f1462fc23cb9158eb056f779432e3b73c3b90
eswar2001 <eswar.tadiparth@juspay.in>
2024-02-29 17:08:42 +0530

    added isCase

--- src/Syn2Chart/Plugin.hs
+++ src/Syn2Chart/Plugin.hs
@@ -46,10 +46,10 @@ buildCfgPass guts = do
             moduleN = moduleNameString $ moduleName $ mg_module guts
             moduleLoc = getFilePath $ mg_loc guts
         res <- translateCoreProgramToCFG binds
-        let tree = foldl (\acc x@(Function name _ _) -> acc Prelude.<> "\n\n\n\n" Prelude.<> (drawTree $ toDataTree x)) "" res
+        let tree = foldl (\acc x@(Function name _ _ _) -> acc Prelude.<> "\n\n\n\n" Prelude.<> (drawTree $ toDataTree x)) "" res
         _ <- Prelude.writeFile (moduleLoc Prelude.<> ".syn.log") tree
         _ <- Data.ByteString.Lazy.writeFile (moduleLoc Prelude.<> ".syn.json") 
-                (encodePretty $ Map.fromList $ map (\t@(Function name _type x) -> (name,t)) res)
+                (encodePretty $ Map.fromList $ map (\t@(Function name _type _ x) -> (name,t)) res)
         pure ()
     return guts
 
@@ -57,29 +57,29 @@ getFilePath :: SrcSpan -> String
 getFilePath (RealSrcSpan rSSpan) = unpackFS $ srcSpanFile rSSpan
 getFilePath (UnhelpfulSpan fs) =  unpackFS fs
 
-data Function = Function String String [Function]
+data Function = Function String String Bool [Function]
     deriving (Show)
 
 instance ToJSON Function where
-    toJSON (Function name _type f') = 
+    toJSON (Function name _type isCase f') =
         Object $ 
-            HM.fromList [("name",toJSON name),("body",toJSON f'),("type",toJSON _type)]
+            HM.fromList [("name",toJSON name),("body",toJSON f'),("type",toJSON _type),("isCase",toJSON isCase)]
 
-instance Semigroup Function where
-    (Function name1 type1 children1) <> (Function name2 type2 children2) =
-        if name1 == "" || name2 == "" then
-            Function (name1 ++ name2) (type1 ++ type2) (children1 Prelude.<> children2)
-        else
-            Function (name1 ++ "__" ++ name2) (type1 ++ "__" ++ type2) (children1 Prelude.<> children2)
+-- instance Semigroup Function where
+--     (Function name1 type1 isCase1 children1) <> (Function name2 type2 isCase2 children2) =
+--         if name1 == "" || name2 == "" then
+--             Function (name1 ++ name2) (type1 ++ type2) (isCase1 && isCase2) (children1 Prelude.<> children2)
+--         else
+--             Function (name1 ++ "__" ++ name2) (type1 ++ "__" ++ type2) (isCase1 && isCase2) (children1 Prelude.<> children2)
 
-instance Monoid Function where
-    mempty = Function "" "" []
+-- instance Monoid Function where
+--     mempty = Function "" "" False []
 
 toDataTree :: Function -> Tree String
-toDataTree (Function name _type children) = Node (name ++ " :: @" ++ _type) (map toDataTree children)
+toDataTree (Function name _type isCase children) = Node (name ++ " :: @" ++ _type) (map toDataTree children)
 
 treeToString :: Function -> String
-treeToString (Function name _type children) = name ++ " ::@" ++ _type ++ "\n" ++ concatMap (\child -> "  " ++ treeToString child) children
+treeToString (Function name _type isCase children) = name ++ " ::@" ++ _type ++ "\n" ++ concatMap (\child -> "  " ++ treeToString child) children
 
 translateCoreProgramToCFG :: [Bind CoreBndr] -> IO [Function]
 translateCoreProgramToCFG r =
@@ -91,7 +91,7 @@ getPivotName = nameStableString . idName
 traverseForFlows :: CoreExpr -> [Function]
 traverseForFlows xx@(Var x) =
     let name = (getPivotName x)
-    in [Function name (showSDocUnsafe $ ppr $ tyVarKind x) []]
+    in [Function name (showSDocUnsafe $ ppr $ tyVarKind x) False []]
 traverseForFlows (Lit x) = []
 traverseForFlows t@(Type _) = []
 traverseForFlows (Coercion _) = []
@@ -99,27 +99,27 @@ traverseForFlows (App f a) =
     let arg = traverseForFlows a
         fun = case traverseForFlows f of
                 [] -> []
-                [(Function name _type [])] -> [Function name _type arg]
-                [(Function name _type x)] -> [Function name _type (arg Prelude.<> x)]
+                [(Function name _type isCase [])] -> [Function name _type isCase arg]
+                [(Function name _type isCase x)] -> [Function name _type isCase (arg Prelude.<> x)]
                 x -> x
     in fun
 traverseForFlows (Lam e a) =
     let !_ = (getPivotName e)
-    in [Function (getPivotName e) "" (traverseForFlows a)]
+    in [Function (getPivotName e) "" False (traverseForFlows a)]
 traverseForFlows (Let b e) = traverseForFlows e ++ countBindingsInternal b
 traverseForFlows (Case e x t alts) = (concat (map (countAlt t) alts))
 traverseForFlows (Cast e _) = traverseForFlows e
 traverseForFlows (Tick _ e) = traverseForFlows e
 
 countBindings :: CoreBind -> [Function]
-countBindings (NonRec binds expr) = [Function (getPivotName binds) (showSDocUnsafe $ ppr $ tyVarKind binds) $ traverseForFlows expr]
+countBindings (NonRec binds expr) = [Function (getPivotName binds) (showSDocUnsafe $ ppr $ tyVarKind binds) False $ traverseForFlows expr]
 countBindings (Rec bs) =
-    map (\(binds,expr) -> Function (getPivotName binds) (showSDocUnsafe $ ppr $ tyVarKind binds) $ traverseForFlows expr) bs
+    map (\(binds,expr) -> Function (getPivotName binds) (showSDocUnsafe $ ppr $ tyVarKind binds) False $ traverseForFlows expr) bs
 
 countBindingsInternal :: CoreBind -> [Function]
 countBindingsInternal (NonRec binds expr) = traverseForFlows expr
 countBindingsInternal (Rec bs) = concat (map (traverseForFlows . snd) bs)
 
 countAlt :: Type -> (AltCon, [Var], CoreExpr) -> [Function]
-countAlt t (p, [], e) = [Function (showSDocUnsafe $ ppr p) (showSDocUnsafe $ ppr t) $ traverseForFlows e]
-countAlt t (p, val, e) = [Function ((showSDocUnsafe $ ppr p) Prelude.<> " <<->> " Prelude.<> (showSDocUnsafe $ ppr val)) (showSDocUnsafe $ ppr t) $ traverseForFlows e]
+countAlt t (p, [], e) = [Function (showSDocUnsafe $ ppr p) (showSDocUnsafe $ ppr t) True $ traverseForFlows e]
+countAlt t (p, val, e) = [Function ((showSDocUnsafe $ ppr p) Prelude.<> " <<->> " Prelude.<> (showSDocUnsafe $ ppr val)) (showSDocUnsafe $ ppr t) True $ traverseForFlows e]
