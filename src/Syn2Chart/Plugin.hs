26d6a057046293e12340e5f612087fa82eb92adc
eswar2001 <eswar.tadiparth@juspay.in>
2024-03-05 17:38:18 +0530

    added AST print support && dumping json

--- src/Syn2Chart/Plugin.hs
+++ src/Syn2Chart/Plugin.hs
@@ -1,34 +1,29 @@
-{-# LANGUAGE FlexibleContexts,BangPatterns,FlexibleInstances,UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Syn2Chart.Plugin (plugin) where
 
-import System.Directory
-import System.FilePath
-import Control.Concurrent
 import Control.Monad
 import CoreMonad
-import CoreStats
 import CoreSyn hiding (TB)
 import Data.Aeson
 import Data.Aeson.Encode.Pretty (encodePretty)
-import Data.ByteString.Lazy (writeFile)
+import Data.ByteString.Lazy (writeFile, fromStrict, toStrict)
 import Data.Data
-import Data.List.Extra (nub,isInfixOf)
 import Data.Maybe
-import Data.Tree
-import Debug.Trace (traceShowId)
-import Demand
-import GHC.Generics (Generic)
 import GhcPlugins hiding (TB,(<>))
-import Module
-import Outputable
-import qualified Data.ByteString.Lazy as BS
-import qualified Data.Functor
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Map as Map
-import qualified Data.Text as Text
-import Var
 import Data.Bool
+import Data.Text (pack)
+import Prelude hiding (id)
+import Control.Concurrent (forkIO)
+import Data.Text.Encoding (decodeUtf8)
+import Data.Text (unpack)
+import qualified Data.ByteString.Base64 as BS
+import System.IO 
+import System.IO.Extra (openFile)
 
 
 plugin :: Plugin
@@ -47,12 +42,21 @@ buildCfgPass guts = do
             moduleN = moduleNameString $ moduleName $ mg_module guts
             moduleLoc = getFilePath $ mg_loc guts
         res <- translateCoreProgramToCFG binds
-        let tree = foldl (\acc x@(Function name _ _ _) -> acc Prelude.<> "\n\n\n\n" Prelude.<> (drawTree $ toDataTree x)) "" res
-        _ <- Prelude.writeFile (moduleLoc Prelude.<> ".syn.log") tree
-        _ <- Data.ByteString.Lazy.writeFile (moduleLoc Prelude.<> ".syn.json") 
-                (encodePretty $ Map.fromList $ map (\t@(Function name _type _ x) -> (name,t)) res)
+        let l = map (\t@(Function name _type _ _) -> (name,t)) res
+        foldM (\acc (functionName,functionbody) -> do
+                        val <- fileWrite (moduleLoc Prelude.<> (unpack $ decodeUtf8 $ BS.encode $ toStrict $ encode functionName) Prelude.<> ".syn.json") (unpack $ decodeUtf8 $ toStrict $ encodePretty functionbody)
+                        pure ([val] ++ acc)
+                      ) [] l
+        !_ <- Prelude.writeFile (moduleLoc Prelude.<> ".ast.show.json")
+                (unpack $ decodeUtf8 $ toStrict $ encode $ Map.fromList (concatMap bindToJSON binds))
         pure ()
     return guts
+
+fileWrite :: String -> String -> IO ()
+fileWrite fileName content = do
+  handle <- openFile fileName WriteMode
+  hPutStr handle content
+  hClose handle
 
 getFilePath :: SrcSpan -> String
 getFilePath (RealSrcSpan rSSpan) = unpackFS $ srcSpanFile rSSpan
@@ -63,54 +67,98 @@ data Function = Function String String Bool [Function]
 
 instance ToJSON Function where
     toJSON (Function name _type isCase f') =
-        Object $ 
+        Object $
             HM.fromList [("name",toJSON name),("body",toJSON f'),("type",toJSON _type),("isCase",toJSON isCase)]
 
--- instance Semigroup Function where
---     (Function name1 type1 isCase1 children1) <> (Function name2 type2 isCase2 children2) =
---         if name1 == "" || name2 == "" then
---             Function (name1 ++ name2) (type1 ++ type2) (isCase1 && isCase2) (children1 Prelude.<> children2)
---         else
---             Function (name1 ++ "__" ++ name2) (type1 ++ "__" ++ type2) (isCase1 && isCase2) (children1 Prelude.<> children2)
+instance ToJSON Var where
+  toJSON var = String $ pack $ nameStableString (idName var)
 
--- instance Monoid Function where
---     mempty = Function "" "" False []
+instance ToJSON (Expr Var) where
+  toJSON (Var id) = toJSON id
+  toJSON (Type id) = String $ pack $ showSDocUnsafe $ ppr id
+  toJSON x@(Lit _) =
+    object
+      [ "type" .= String "Lit",
+        "literal" .= pack (showSDocUnsafe (ppr x))
+      ]
+  toJSON (App (Var id) (Type t)) =
+    object
+      [ "type" .= String "App",
+        "function" .= toJSON id,
+        "argument" .= String (pack $ showSDocUnsafe $ ppr t)
+      ]
+  toJSON (App fun (Type _)) = toJSON fun
+  toJSON (App fun arg) =
+    object
+      [ "type" .= String "App",
+        "function" .= toJSON fun,
+        "argument" .= toJSON arg
+      ]
+  toJSON (Lam bind expr) =
+    object
+      [ "type" .= String "Lam",
+        "function" .= toJSON bind,
+        "argument" .= toJSON expr
+      ]
+  toJSON (Let bind expr) =
+    object
+      [ "type" .= String "Let",
+        "function_list" .=
+            let bindsList = bindToJSON bind
+            in Map.fromList bindsList,
+        "argument" .= toJSON expr
+      ]
+  toJSON (Case condition bind _type alts) =
+    object
+      [ "type" .= String "Case",
+        "condition_type" .= String (pack $ showSDocUnsafe $ ppr _type),
+        "condition" .= toJSON condition,
+        "function" .= toJSON bind,
+        "matches" .= toJSON alts
+      ]
+  toJSON v = object ["unhandled" .= String (pack $ show $ toConstr v),"value" .= String (pack $ showSDocUnsafe $ ppr v)]
 
-toDataTree :: Function -> Tree String
-toDataTree (Function name _type isCase children) = Node (name ++ " :: @" ++ _type) (map toDataTree children)
+instance ToJSON AltCon where
+  toJSON (DataAlt dataCon) = String (pack $ showSDocUnsafe $ ppr dataCon)
+  toJSON (LitAlt lit) = String (pack $ showSDocUnsafe $ ppr lit)
+  toJSON DEFAULT = String "DEFAULT"
 
-treeToString :: Function -> String
-treeToString (Function name _type isCase children) = name ++ " ::@" ++ _type ++ "\n" ++ concatMap (\child -> "  " ++ treeToString child) children
+bindToJSON :: CoreBind -> [(String, Value)]
+bindToJSON (NonRec binder ((Lam _ expr))) =
+  [(nameStableString (idName binder), toJSON expr)]
+bindToJSON (NonRec binder expr) =
+  [(nameStableString (idName binder), toJSON expr)]
+bindToJSON (Rec binds) =
+  map (\(b, e) -> (nameStableString (idName b), toJSON e)) binds
 
 translateCoreProgramToCFG :: [Bind CoreBndr] -> IO [Function]
 translateCoreProgramToCFG r =
-    pure $ concat $ map countBindings r
+    pure $ concatMap countBindings r
 
 getPivotName :: Var -> String
 getPivotName var = nameStableString $ idName var
 
 traverseForFlows :: CoreExpr -> [Function] -> [Function]
-traverseForFlows xx@(Var x) _ =
-    let name = (getPivotName x)
-    in ([Function name (showSDocUnsafe $ ppr $ tyVarKind x) False []])
-traverseForFlows (Lit x) _ = []
-traverseForFlows t@(Type _) _ = []
-traverseForFlows (Coercion _) _ = []
+traverseForFlows (Var x) argument =
+    let name = getPivotName x
+    in [Function name (showSDocUnsafe $ ppr $ tyVarKind x) False argument]
+traverseForFlows (Lit _) argument = argument
+traverseForFlows (Type _) argument = argument
+traverseForFlows (Coercion _) argument = argument
 traverseForFlows (App (Var id) (Lit x)) argument = [Function (getPivotName id) "" False [Function (showSDocUnsafe $ ppr x) "" False argument]]
-traverseForFlows (App (Var id) (Var x)) argument = [Function (getPivotName id) "" False [Function (traceShowId $ getPivotName x) "" False argument]]
--- traverseForFlows (App f (Type x)) = traverseForFlows f
-traverseForFlows (App (Var id) (Type x)) argument = [Function (getPivotName id) "" False argument]
+traverseForFlows (App (Var id) (Var x)) argument = [Function (getPivotName id) "" False [Function (getPivotName x) "" False argument]]
+traverseForFlows (App (Var id) (Type _)) argument = [Function (getPivotName id) "" False argument]
 traverseForFlows (App f a) argument =
-    (let arg = traverseForFlows (trace (show $ toConstr a) a) argument
-         fun = traverseForFlows f arg
-    in fun)
-traverseForFlows x@(Lam e a) argument =
-    let !_ = (getPivotName e)
-    in [Function (getPivotName e) "" False (traverseForFlows a argument)]
-traverseForFlows x@(Let b e) _ = ((traverseForFlows e []) ++ countBindingsInternal b)
-traverseForFlows (Case e x t alts) _ = (concat (map (countAlt t) alts))
-traverseForFlows x@(Cast e _) _ = ((traverseForFlows e []))
-traverseForFlows x@(Tick _ e) _ = ((traverseForFlows e []))
+    let arg = traverseForFlows a argument
+        fun = traverseForFlows f arg
+   in fun
+traverseForFlows (Lam e a) argument = [Function (getPivotName e) "" False (traverseForFlows a argument)]
+traverseForFlows (Let b e) argument = 
+  let arg = traverseForFlows e argument
+  in countBindingsInternal b arg
+traverseForFlows (Case e x t alts) argument = concatMap (\x -> countAlt t x argument) alts
+traverseForFlows (Cast e _) argument = traverseForFlows e argument
+traverseForFlows (Tick _ e) argument = traverseForFlows e argument
 
 countBindings :: CoreBind -> [Function]
 countBindings (NonRec binds expr) =
@@ -119,13 +167,13 @@ countBindings (NonRec binds expr) =
 countBindings (Rec bs) =
   mapMaybe (\(binds,expr) ->
     let maybeName = Just (nameStableString $ idName binds)
-    in maybe Nothing (\name -> Just $ Function name (showSDocUnsafe $ ppr $ tyVarKind binds) False $ traverseForFlows expr []) maybeName) bs
+    in (\name -> Just $ Function name (showSDocUnsafe $ ppr $ tyVarKind binds) False $ traverseForFlows expr []) =<< maybeName) bs
 
-countBindingsInternal :: CoreBind -> [Function]
-countBindingsInternal (NonRec binds expr) = traverseForFlows expr []
-countBindingsInternal (Rec bs) = concat (map ((\x -> traverseForFlows x []) . snd) bs)
+countBindingsInternal :: CoreBind -> [Function] -> [Function]
+countBindingsInternal (NonRec _ expr) argument = traverseForFlows expr argument
+countBindingsInternal (Rec bs) argument = concatMap ((`traverseForFlows` argument) . snd) bs
 
-countAlt :: Type -> (AltCon, [Var], CoreExpr) -> [Function]
-countAlt t (p, [], e) = [Function (showSDocUnsafe $ ppr p) (showSDocUnsafe $ ppr t) True $ traverseForFlows e []]
-countAlt t (p, val, e) = [Function ((showSDocUnsafe $ ppr p) Prelude.<> " <<->> " Prelude.<> (showSDocUnsafe $ ppr val)) (showSDocUnsafe $ ppr t) True $ traverseForFlows e []]
+countAlt :: Type -> (AltCon, [Var], CoreExpr) -> [Function] -> [Function]
+countAlt t (p, [], e) args = [Function (showSDocUnsafe $ ppr p) (showSDocUnsafe $ ppr t) True $ traverseForFlows e args]
+countAlt t (p, val, e) args = [Function (showSDocUnsafe $ ppr p) (showSDocUnsafe $ ppr t) True $ traverseForFlows e args]
 
